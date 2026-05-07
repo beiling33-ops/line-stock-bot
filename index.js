c54a2a09885c6bef58747881401efe143f8b07f0
beiling33-ops <bei.ling33@gmail.com>
2026-05-07 19:25:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,34 @@ const line = require('@line/bot-sdk');
 
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelSecret: process.env.CHANNEL_SECRET
 };
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('LINE BOT OK');
+});
+
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
 const client = new line.Client(config);
 
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+
+  if (event.type !== 'message') {
+    return Promise.resolve(null);
+  }
+
+  if (event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
